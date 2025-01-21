f0d84455719c8f4c0e7e28510dbb674878f2e4ea
Krishpabhattarai <krishpabhattarai@mail.weber.edu>
2025-01-21 09:18:23 -0700

    ADD: Docstring for program info

--- game.py
+++ game.py
@@ -1,2 +1,11 @@
+"""
+Adventure Game
+Author: Krishpa Bhattarai
+Version: 1.0
+Description:
+This is a text-based adventure game where the player makes choices
+to navigate through a mysterious forest.
+"""
+
 print("Welcome to the Adevnture Game!")
 print("Your journey begins here...")
